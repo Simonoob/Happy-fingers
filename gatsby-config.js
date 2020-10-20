46d06d9e508a69f7aa50eeffd7c147cb65510846
Simone Fiorentini <simonefiorentini1@gmail.com>
2020-10-20 15:45:21 +0300

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `🖐 Happy fingers`,
+    defaultTitle: `🖐 Happy fingers`,
+    siteTitleShort: `Happy fingers`,
+    siteDescription: `Finger pulley prevention resource`,
+    siteUrl: `https://rocketdocs.netlify.com`,
+    siteAuthor: `@SimoneFiorentini`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#000000`,
+    basePath: `/`,
+  },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        githubUrl: `https://github.com/rocketseat/gatsby-themes`,
+        baseDir: `examples/gatsby-theme-docs`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: ``,
+      },
+    },
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.com`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
---
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    primary: "#7AB2E1",
+    background: "#FFFFFF",
+    shape: `#F2F2FA`,
+    title: `#3D3D4D`,
+    text: `#6C6C80`,
+  },
+
+};
